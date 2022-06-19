cff2f83af89a13bbd5bc01f3832672d0b0f6f7d0
umahanty <106217275+umahanty@users.noreply.github.com>
2022-06-19 20:02:43 +0530

    Add files via upload

---
+++ cartisenusc1.sh
@@ -0,0 +1,21 @@
+package javapp;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x1,y1,x2,y2;
+        double dis;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter point x1: ");
+        x1 = sc.nextInt();
+        System.out.print("Enter point x2: ");
+        x2 = sc.nextInt();
+        System.out.print("Enter y1 point: ");
+        y1 = sc.nextInt();
+        System.out.print("Enter y2 point: ");
+        y2 = sc.nextInt();
+        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.print("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
+    }
+}
