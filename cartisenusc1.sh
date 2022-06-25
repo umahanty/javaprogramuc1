edbe453f540fa3fcbf41f8b122c6b43967a74ea7
umahanty <106217275+umahanty@users.noreply.github.com>
2022-06-25 09:37:15 +0530

    Update cartisenusc1.sh

--- cartisenusc1.sh
+++ cartisenusc1.sh
@@ -15,7 +15,7 @@ public class Main {
         y1 = sc.nextInt();
         System.out.print("Enter y2 point: ");
         y2 = sc.nextInt();
-        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)); // Math. sqrt() is used to return the square root of a number.
         System.out.print("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
     }
 }
